c5237ff14e261a66262fec3612a374191978c3a7
chris kenyon <62793208+chriskenyon1991@users.noreply.github.com>
2021-04-25 00:01:21 +0100

    fixing bugs in API finnhub code

--- components/API/finnhub.js
+++ components/API/finnhub.js
@@ -1,43 +1,56 @@
-const finnhub = require("finnhub");
+// const { default: axios } = require("axios");
+// const finnhub = require("finnhub");
 
-const api_key = finnhub.ApiClient.instance.authentications["api_key"];
-api_key.apiKey = "sandbox_c0lsa3f48v6r1vcsdurg"; // Replace this
-const finnhubClient = new finnhub.DefaultApi();
+// const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+// api_key.apiKey = "sandbox_c0lsa3f48v6r1vcsdurg"; // Replace this
+// const finnhubClient = new finnhub.DefaultApi();
 
-function getLineChartData(stockTicker, resoloution, from, to) {
-  finnhubClient.stockCandles(
-    stockTicker,
-    resoloution,
-    from,
-    to,
-    {},
-    (error, data, response) => {
-      console.log(data.o);
-      return data.o;
-    }
-  );
+function price(stockTicker) {
+  axios
+    .get(
+      "https://finnhub.io/api/v1/quote?symbol=AAPL&token=sandbox_c0lsa3f48v6r1vcsdurg"
+    )
+    .then((response) => {
+      console.log(response.data.c);
+      return response.data.c;
+    });
 }
+module.exports = { price };
+// exports.getLineChartData = (stockTicker, resoloution, from, to) => {
+//   finnhubClient.stockCandles(
+//     stockTicker,
+//     resoloution,
+//     from,
+//     to,
+//     {},
+//     (error, data, response) => {
+//       console.log(data.o);
+//       return data.o;
+//     }
+//   );
+// };
 
-function getCompanyProfile(stockTicker) {
-  // Company profile
-  finnhubClient.companyProfile(
-    { symbol: stockTicker },
-    (error, data, response) => {
-      console.log("vv company profile vv");
-      console.log(data);
-    }
-  );
-}
+// exports.getCompanyProfile = (stockTicker) => {
+//   // Company profile
+//   finnhubClient.companyProfile(
+//     { symbol: stockTicker },
+//     (error, data, response) => {
+//       console.log("vv company profile vv");
+//       console.log(data);
+//       return data;
+//     }
+//   );
+// };
 
-function getCurrentPrice(stockTicker) {
-  finnhubClient.quote("AAPL", (error, data, response) => {
-    let currentPrice = data.c;
-    console.log("vv current price vv");
-    console.log(currentPrice);
-    return currentPrice;
-  });
-}
+// exports.getCurrentPrice = (stockTicker) => {
+//   finnhubClient.quote("AAPL", (error, data, response) => {
+//     let currentPrice = data.c;
+//     console.log("vv current price vv");
+//     console.log(currentPrice);
+//     return currentPrice;
+//   });
+// };
 
-getLineChartData("AAPL", "D", 1590988249, 1591852249);
-getCompanyProfile("TSLA");
-getCurrentPrice("TSLA");
+// getLineChartData("AAPL", "D", 1590988249, 1591852249);
+// getCompanyProfile("TSLA");
+// getCurrentPrice("TSLA");
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,79 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-native';
-import {Card, Title, Paragraph} from 'react-native-paper'
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
+import { Card, Title, Paragraph } from "react-native-paper";
+
+import price from "./API/finnhub";
 
 export default Home = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.polygon.io/v2/reference/tickers?sort=ticker&market=STOCKS&perpage=50&page=1&apiKey=3g7pzSJLIdyzg2UUHtZqeHtfeEnpMjqX')
-      .then((response) => response.json())
-      .then((json) => setData(json.data))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
     <View style={styles.container}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}          
-          keyExtractor={({ tickers }, index) => tickers}
-          renderItem={({ item }) => (
-              <Card style={styles.card}>
-                <Card.Content>
-                    <Title>{item.ticker}</Title>              
-                </Card.Content>               
-             </Card>
-          )}
-        />
-      )}
+      <price />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
-  container:{
-      backgroundColor:'whitesmoke'
+  container: {
+    backgroundColor: "whitesmoke",
   },
-  card:{
-      margin:2,
-      backgroundColor: 'white',
-      color: 'white',
-      borderWidth:2,
-      borderColor: 'whitesmoke',
-
+  card: {
+    margin: 2,
+    backgroundColor: "white",
+    color: "white",
+    borderWidth: 2,
+    borderColor: "whitesmoke",
   },
-  text:{
-      display:'flex',
-      color: '#111',
-      fontSize:14,
-      fontFamily:'Futura',
-      padding:10,
-      justifyContent:'space-between'
+  text: {
+    display: "flex",
+    color: "#111",
+    fontSize: 14,
+    fontFamily: "Futura",
+    padding: 10,
+    justifyContent: "space-between",
   },
-  open:{
-      color: '#111',
-      fontSize:20,
-      textAlign:'left',
-      fontFamily:'Futura'
+  open: {
+    color: "#111",
+    fontSize: 20,
+    textAlign: "left",
+    fontFamily: "Futura",
   },
-  close:{
-      color: '#111',
-      fontSize:20,
-      textAlign:'right',
-      fontFamily:'Futura'
+  close: {
+    color: "#111",
+    fontSize: 20,
+    textAlign: "right",
+    fontFamily: "Futura",
   },
-  closeOpen:{
-      display:'flex',
-      flexDirection:'row',
-      justifyContent: 'space-between'
+  closeOpen: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  stockImage:{
-      width: 40,
-      height: 40,
-  }
-
+  stockImage: {
+    width: 40,
+    height: 40,
+  },
 });
